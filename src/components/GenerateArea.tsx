b0f8891548b412e8424422fbb70ef8a78cd23a7c
traviolus <david.tumcharoen@gmail.com>
2021-05-02 04:39:43 +0700

    Fix bugs

--- src/components/GenerateArea.tsx
+++ src/components/GenerateArea.tsx
@@ -27,10 +27,13 @@ export default function GenerateArea() {
             start_text: startText,
             page: page
         };
-        const response = await axios.get('http://35.198.207.198:8000/api/sentence/', {
+        await axios.get('http://35.198.207.198:8000/api/sentence/', {
             params: payload
+        }).then((response) => {
+            setText(response.data.message);
+        }).catch((error) => {
+            setText('คำเริ่มต้นไม่ถูกต้อง กรุณาลองใหม่');
         });
-        setText(response.data.message);
         setIsLoading(false);
     }
 
@@ -43,22 +46,22 @@ export default function GenerateArea() {
                 เลือกสไตล์ที่ต้องการและกรอกข้อความเริ่มต้น
             </h1>
             <Input.Group compact>
-            <Select defaultValue="เลือกสไตล์" style={{ width: '10%', }} size='large' value={page} onChange={(e) => handlePage(e.toString())}>
-                <Option value="dinosaturday">ไดโนเศร้า</Option>
-                <Option value="t047">t_047</Option>
-                <Option value="quotes">คำคม</Option>
-                <Option value="mix">แบบผสม!</Option>
-            </Select>
-            <Search 
-                loading={isLoading} 
-                placeholder='ป้อนข้อความเริ่มต้นที่นี่' 
-                onChange={(e) => handleText(e.target.value)} 
-                onSearch={generateText} 
-                enterButton="Search" 
-                size="large" 
-                style={{ width: '50%' }} 
-                value={startText} 
-            />
+                <Select defaultValue="เลือกสไตล์" style={{ width: '10%', }} size='large' value={page} onChange={(e) => handlePage(e.toString())}>
+                    <Option value="dinosaturday">ไดโนเศร้า</Option>
+                    <Option value="t047">t_047</Option>
+                    <Option value="quotes">คำคม</Option>
+                    <Option value="mix">แบบผสม!</Option>
+                </Select>
+                <Search 
+                    loading={isLoading} 
+                    placeholder='ป้อนข้อความเริ่มต้นที่นี่' 
+                    onChange={(e) => handleText(e.target.value)} 
+                    onSearch={generateText} 
+                    enterButton="Generate" 
+                    size="large" 
+                    style={{ width: '50%' }} 
+                    value={startText} 
+                />
             </Input.Group>
             <h2 className='result' style={{'margin': '30px auto 0px auto', width: '80%'}}>
                 {text}
